59bc23d6900885e98132ce422009992fdf546f2a
Refi Lexmana <refi.lexmana13@gmail.com>
2026-02-26 11:58:01 +0700

    Test 1

--- src/test/java/com/refi/api/baseApi/baseApi.java
+++ src/test/java/com/refi/api/baseApi/baseApi.java
@@ -1,18 +1,17 @@
 package com.refi.api.baseApi;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class baseApi {
     private String baseUrl= "https://dummyapi.io";
-    private static final Dotenv dotenv = Dotenv.load();
-    private String apiKey = dotenv.get("app-id");
+    private String appId = "63a804408eb0cb069b57e43a";
 
     public Response getListUser(){
         return RestAssured.given()
                 .baseUri(baseUrl)
-                .header("app-id", apiKey)
+                .header("app-id", appId)
                 .when()
                 .get(baseUrl + "/data/v1/user");
     }
